87d46be8c227d5e34ad745fffe4f83a7ce332c9b
andrew-wulf <andrewdwulf@gmail.com>
2024-05-30 12:17:17 -0500

    authenticated localhost5173, index jbuilder changes

--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! product, :id, :name, :price, :description, :category, :on_sale, :image_url
+json.extract! product, :id, :name, :price, :description, :categories, :in_stock, :on_sale, :color, :material, :weight, :supplier
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "mini-capstone.peterxjang.com/restful-routes"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
+
+  #let's see what happens
+  allow do
+    origins "http://localhost:5173"
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
+  end
 end
